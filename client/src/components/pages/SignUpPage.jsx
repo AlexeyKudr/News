02e80f1db2e9c79eda8fcee411dfd8d89d83f55b
Dava13 <dava-13-05@mail.ru>
2024-06-19 19:36:39 +0300

    update

--- client/src/components/pages/SignUpPage.jsx
+++ client/src/components/pages/SignUpPage.jsx
@@ -3,30 +3,24 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import React from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
-export default function SignUpPage({signUpHandler}) {
-export default function SignUpPage({signUpHandler}) {
+export default function SignUpPage({ signUpHandler }) {
   return (
     <Row>
       <Col md={{ span: 6, offset: 3 }} className="mt-5">
-      <h3 className="text-center">Регистрация</h3>
+        <h3 className="text-center">Регистрация</h3>
         <Form onSubmit={signUpHandler}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Имя</Form.Label>
-            <Form.Control
-              name="name"
-              type="text"
-              placeholder="Введите имя"
-            />
+            <Form.Control name="name" type="text" placeholder="Введите имя" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Почта</Form.Label>
-            <Form.Control name="email" type="email" placeholder="Введите почту" />
+            <Form.Control
+              name="email"
+              type="email"
+              placeholder="Введите почту"
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Пароль</Form.Label>
