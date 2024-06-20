ca8903b7892228415baf95f56bb9fc61fa5ec03e
AlexeyKudr <renenkton123@yandex.ru>
2024-06-20 12:18:55 +0300

    update

--- client/src/components/ui/NewsCard.jsx
+++ client/src/components/ui/NewsCard.jsx
@@ -6,8 +6,9 @@ export default function NewsCard({ card, handleMore, handleFavorite }) {
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Body>
-        <Card.Title>{card.name}</Card.Title>
-        <Card.Text>{card.discription}</Card.Text>
+      <Card.Img variant="top" src={card.image} />
+        <Card.Title>{card.title}</Card.Title>
+        <Card.Text>{card.preview}</Card.Text>
         <Button variant="primary" onClick={() => handleMore(card.id)}>Подробнее</Button>
         <Button variant="primary" onClick={() => handleFavorite(card.id)}>Добавить в избранное</Button>
       </Card.Body>
--- server/articles.js
+++ server/articles.js
@@ -2,8 +2,8 @@ const puppeteer = require('puppeteer');
 
 const getArticles = async () => {
   const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 
   const page = await browser.newPage();
