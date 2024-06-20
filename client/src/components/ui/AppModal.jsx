620171fa9332b28f5bf2240d705f218cba3ea77e
DariaVor <daria_voronina@list.ru>
2024-06-20 13:44:06 +0300

    modal

--- client/src/components/ui/AppModal.jsx
+++ client/src/components/ui/AppModal.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
-import { Button, Modal } from 'react-bootstrap';
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
-export default function AppModal({title, children, buttonText}) {
-    const [show, setShow] = useState(false);
+export default function AppModal({ title, children, buttonText }) {
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
-    return (
-      <>
-        <Button variant="primary" className="m-2" onClick={handleShow}>
-          {buttonText}
-        </Button>
-  
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>{title}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>{children}</Modal.Body>
-        </Modal>
-      </>
-    );
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" className="m-2" onClick={handleShow}>
+        {buttonText}
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{children}</Modal.Body>
+      </Modal>
+    </>
+  );
 }
--- client/src/components/ui/NewsCard.jsx
+++ client/src/components/ui/NewsCard.jsx
@@ -10,7 +10,14 @@ export default function NewsCard({ card, handleMore, handleFavorite }) {
       <Card.Img variant="top" src={card.image} />
         <Card.Title>{card.title}</Card.Title>
         <Card.Text>{card.preview}</Card.Text>
-        <Button variant="primary" onClick={() => handleMore(card.id)}>Подробнее</Button>
+        <AppModal title="Подробнее" buttonText="Подробнее">
+          <div className="text-center">
+            <Card.Title>{card.title}</Card.Title>
+            <Card.Img variant="top" src={card.image} />
+            <Card.Text>{card.description}</Card.Text>
+          </div>
+        </AppModal>
+        {/* <Button variant="primary" onClick={() => handleMore(card.id)}>Подробнее</Button> */}
         <Button variant="primary" onClick={() => handleFavorite(card.id)}>Добавить в избранное</Button>
       </Card.Body>
     </Card>
