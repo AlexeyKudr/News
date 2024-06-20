077672a4985fefd294dd98bd0fb60fae036b83ed
ViktorLid <viks130293@gmail.com>
2024-06-20 18:48:00 +0300

    update

--- client/src/components/pages/NewsPage.jsx
+++ client/src/components/pages/NewsPage.jsx
@@ -4,7 +4,7 @@ import NewsWrapper from '../ui/NewsWrapper';
 import axiosInstance from '../api/axiosInstance';
 import Filter from '../ui/Flter';
 
-export default function NewsPage({user}) {
+export default function NewsPage({ user }) {
   const [cards, setCards] = useState([]);
   const [favoriteNewsIds, setFavoriteNewsIds] = useState(new Set());
   const [keyword, setKeyword] = useState('');
@@ -19,36 +19,36 @@ export default function NewsPage({user}) {
         console.error('Error fetching news:', error);
       }
     };
-
     fetchNews(); // Вызываем функцию загрузки новостей при загрузке компонента
-    axiosInstance.get("/account")
+  }, []);
+
+  useEffect(() => {
+    axiosInstance.get('/news').then((res) => setCards(res.data));
+    axiosInstance
+      .get('/account')
       .then((res) => {
-        const favoriteIds = new Set(res.data.map(news => news.id));
+        const favoriteIds = new Set(res.data.map((news) => news.id));
         setFavoriteNewsIds(favoriteIds);
       })
-      .catch((error) => console.error("Error fetching favorite news", error));
+      .catch((error) => console.error('Error fetching favorite news', error));
   }, []);
 
   const handleFavorite = async (id) => {
     try {
-      const response = await axiosInstance.post("/account", {
+      const response = await axiosInstance.post('/account', {
         userId: user.id,
-        newsId: id
+        newsId: id,
       });
       if (response.status === 201) {
         setFavoriteNewsIds((prevSet) => new Set(prevSet).add(id));
-        console.log("Новость добавлена в избранное");
+        console.log('Новость добавлена в избранное');
       } else {
-        console.error("Ошибка при добавлении новости в избранное");
+        console.error('Ошибка при добавлении новости в избранное');
       }
     } catch (error) {
-      console.error("Ошибка при добавлении новости в избранное:", error);
+      console.error('Ошибка при добавлении новости в избранное:', error);
     }
   };
-  
-  const handleMore = (id) => {};
-
-  const handleFavorite = (id) => {};
 
   const handleFilter = async (e) => {
     e.preventDefault();
@@ -77,8 +77,8 @@ export default function NewsPage({user}) {
       <Row>
         <NewsWrapper
           cards={cards}
-         
-          handleFavorite={handleFavorite} favoriteNewsIds={favoriteNewsIds}
+          handleFavorite={handleFavorite}
+          favoriteNewsIds={favoriteNewsIds}
         />
       </Row>
     </>
--- server/src/routes/news.router.js
+++ server/src/routes/news.router.js
@@ -1,18 +1,18 @@
-const express = require('express');
+
+const express = require('express')
 const { Op } = require('sequelize');
 const { News } = require('../../db/models');
 const { verifyAccessToken } = require('../middlewares/verifyTokens');
+const newsRouter = express.Router();
 
-newsRouter.route('/').get(async (req, res) => {
+newsRouter.route('/').get(verifyAccessToken, async (req, res) => {
   try {
     const posts = await News.findAll();
     res.json(posts);
   } catch (err) {
     res.status(500).send('Internal server error');
   }
-});
-
-newsRouter.post('/', async (req, res) => {
+}).post(verifyAccessToken, async (req, res) => {
   console.log(req.body);
   const { excludeWord, searchString } = req.body;
 
