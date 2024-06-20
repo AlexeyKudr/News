5cc09112882eb4f25da5ae4bbe7b129e8da26fd6
DariaVor <daria_voronina@list.ru>
2024-06-20 12:22:40 +0300

    update

--- client/src/components/ui/NewsCard.jsx
+++ client/src/components/ui/NewsCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import AppModal from "./AppModal";
 
 export default function NewsCard({ card, handleMore, handleFavorite }) {
   return (
@@ -8,6 +9,11 @@ export default function NewsCard({ card, handleMore, handleFavorite }) {
       <Card.Body>
         <Card.Title>{card.name}</Card.Title>
         <Card.Text>{card.discription}</Card.Text>
+        <AppModal title="Подробнее" buttonText="Подробнее">
+        <div className="text-center">
+          
+        </div>
+        </AppModal>
         <Button variant="primary" onClick={() => handleMore(card.id)}>Подробнее</Button>
         <Button variant="primary" onClick={() => handleFavorite(card.id)}>Добавить в избранное</Button>
       </Card.Body>
